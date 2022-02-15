b4f5235c621504d54cb1aa6b8e38673a1608d98b
hong.chen <hong.chen@audaque.com>
2022-02-15 10:37:40 +0800

    create:项目搭建

---
+++ src/main/java/com/godink/springcloud/log4j2/junit4/demo/SpringbootLog4j2Junit4DemoApplication.java
@@ -0,0 +1,13 @@
+package com.godink.springcloud.log4j2.junit4.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLog4j2Junit4DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLog4j2Junit4DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/godink/springcloud/log4j2/junit4/demo/SpringbootLog4j2Junit4DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godink.springcloud.log4j2.junit4.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLog4j2Junit4DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
